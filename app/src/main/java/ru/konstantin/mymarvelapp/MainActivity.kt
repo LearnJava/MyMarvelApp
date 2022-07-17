7f55c4dbdc865cb0c13dd13f494f019a6d013081
Kuzmin <kuzmin.konstantin@otr.ru>
2022-07-17 19:15:20 +0300

    Init

---
+++ app/src/main/java/ru/konstantin/mymarvelapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.konstantin.mymarvelapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
